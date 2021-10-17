267c4f44fa33c3cc94d763c63ec503db72ea8025
phantomXXI <haianhlol2001@gmail.com>
2021-10-17 23:13:29 +0700

    Áp dụng SoftDelete cho việc Xóa sản phẩm giúp có thể rollback lại dữ liệu

--- app/Product.php
+++ app/Product.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
     protected $guarded = [];
     // Eloquent Relationships: One to many (nhieu anh thuoc ve 1 san pham)
     public function images() {
