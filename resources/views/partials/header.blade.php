b304170532a20a1eabe4cd8090df1d80d4fc6371
phantomXXI <haianhlol2001@gmail.com>
2021-10-22 14:51:34 +0700

    Show Form Search Product

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,28 +1,37 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li>
-    </ul>
 
     <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-        <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </div>
+    <form class="form-inline">
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập ID </label>
+            <input type="text" class="form-control" placeholder="Nhập Id">
         </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tên sản phẩm </label>
+            <input type="text" class="form-control" placeholder="Nhập tên sản phẩm">
+        </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tags sản phẩm </label>
+            <input type="text" class="form-control" placeholder="Nhập tags sản phẩm">
+        </div>
+
+        <div class="form-group mx-sm-2 mb-2">
+            <label class="sr-only"> Nhập tên sản phẩm </label>
+            <select class="form-control">
+                <option value="">Chọn danh mục sản phẩm</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary mb-2">Search</button>
     </form>
 
     <!-- Right navbar links -->
